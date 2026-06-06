2fe5b5a9d5eaf751ef6d9cc43db25d22dc4b699b
Vansh Dhall <vanshdhall918@gmail.com>
2026-06-06 15:42:17 +0530

    fixing connection

--- AI_Travel_Planning_System/database.py
+++ AI_Travel_Planning_System/database.py
@@ -4,24 +4,25 @@ import streamlit as st
 # Database Connection
 DATABASE_URL = st.secrets["DATABASE_URL"]
 
-connection = psycopg.connect(
-    DATABASE_URL,
-    autocommit=True
-)
-
+def get_connection():
+    return psycopg.connect(
+        DATABASE_URL,
+        autocommit=True
+    )
 
 # Create Table
 def create_conversation_table():
-    with connection.cursor() as cur:
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS travel_conversations (
-            id SERIAL PRIMARY KEY,
-            thread_id TEXT NOT NULL,
-            user_query TEXT NOT NULL,
-            final_response TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-        """)
+    with get_connection() as connection:
+        with connection.cursor() as cur:
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS travel_conversations (
+                id SERIAL PRIMARY KEY,
+                thread_id TEXT NOT NULL,
+                user_query TEXT NOT NULL,
+                final_response TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            """)
 
 
 def save_conversation(
@@ -29,22 +30,26 @@ def save_conversation(
     user_query,
     final_response
 ):
-    with connection.cursor() as cur:
+    try:
+        with get_connection() as connection:
+            with connection.cursor() as cur:
 
-        cur.execute(
-            """
-            INSERT INTO travel_conversations
-            (
-                thread_id,
-                user_query,
-                final_response
-            )
+                cur.execute(
+                    """
+                    INSERT INTO travel_conversations
+                    (
+                        thread_id,
+                        user_query,
+                        final_response
+                    )
 
-            VALUES (%s,%s,%s)
-            """,
-            (
-                thread_id,
-                user_query,
-                final_response
-            )
-        )
+                    VALUES (%s,%s,%s)
+                    """,
+                    (
+                        thread_id,
+                        user_query,
+                        final_response
+                    )
+                )
+    except Exception as e:
+        print(f"Database Error: {e}")
